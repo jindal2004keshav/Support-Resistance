cd86d553c4bbbae69e4df166d7283b21550e3adc
Keshav Jindal <114847310+jindal2004keshav@users.noreply.github.com>
2024-07-09 22:03:09 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import yfinance as yf
 import pandas as pd
 import numpy as np
 import seaborn as sns
-from mpl_finance import candlestick_ohlc
+import mplfinance as mpf
 import matplotlib.dates as mpl_dates
 
 app = Flask(__name__)
@@ -22,7 +22,7 @@ def calculate_support_resistance(stock_name, stock_start_date, stock_end_date, i
             return None, None
         
         stock = yf.Ticker(stock_name)
-        df = stock.history(interval=interval,start=start_date,end=end_date)
+        df = stock.history(interval=interval, start=start_date, end=end_date)
         
         if df.empty:
             return None, None
@@ -30,19 +30,19 @@ def calculate_support_resistance(stock_name, stock_start_date, stock_end_date, i
         df['Date'] = pd.to_datetime(df.index)
         df['Date'] = df['Date'].apply(mpl_dates.date2num)
 
-        df = df.loc[:,['Date', 'Open', 'High', 'Low', 'Close']]
-        s =  np.mean(df['High'] - df['Low'])
+        df = df.loc[:, ['Date', 'Open', 'High', 'Low', 'Close']]
+        s = np.mean(df['High'] - df['Low'])
         
-        def isSupport(df,i):
+        def isSupport(df, i):
             support = df['Low'].iloc[i] < df['Low'].iloc[i-1]  and df['Low'].iloc[i] < df['Low'].iloc[i+1] and df['Low'].iloc[i+1] < df['Low'].iloc[i+2] and df['Low'].iloc[i-1] < df['Low'].iloc[i-2]
             return support
 
-        def isResistance(df,i):
-            resistance = df['High'].iloc[i] > df['High'].iloc[i-1]  and df['High'].iloc[i] > df['High'].iloc[i+1] and df['High'].iloc[i+1] > df['High'].iloc[i+2] and df['High'].iloc[i-1] > df['High'].iloc[i-2] 
+        def isResistance(df, i):
+            resistance = df['High'].iloc[i] > df['High'].iloc[i-1]  and df['High'].iloc[i] > df['High'].iloc[i+1] and df['High'].iloc[i+1] > df['High'].iloc[i+2] and df['High'].iloc[i-1] > df['High'].iloc[i-2]
             return resistance
 
-        def isFarFromLevel( l):
-            return np.sum([abs(l-x) < s  for x in levels]) == 0
+        def isFarFromLevel(l):
+            return np.sum([abs(l-x) < s for x in levels]) == 0
 
         levels = []
         for i in range(2, df.shape[0] - 2):
@@ -59,41 +59,25 @@ def calculate_support_resistance(stock_name, stock_start_date, stock_end_date, i
         return df, levels
     except Exception as e:
         print(f"Error in calculate_support_resistance: {e}")
-        return None, 
+        return None, None
 
 def plot_all(df, levels):
     sns.set(style='whitegrid', palette='pastel')
-    fig, ax = plt.subplots()
-
-    candlestick_ohlc(ax,df.values,width=0.6, colorup='green', colordown='red', alpha=0.8)
-
-    date_format = mpl_dates.DateFormatter('%d %b %Y')
-    ax.xaxis.set_major_formatter(date_format)
-    fig.autofmt_xdate()
-
-    min_price = df['Low'].min(skipna=True)
-    max_price = df['High'].max(skipna=True)
-    price_range = max_price - min_price
-    num_ticks = 15  # Choose the desired number of y-ticks
-    y_tick_spacing = round(price_range / num_ticks, -1) if price_range != 0 else 1
-    ax.set_yticks(np.arange(round(min_price / 10) * 10, round(max_price / 10) * 10 + y_tick_spacing, y_tick_spacing))
-
-    ax.set_xlabel('Date')
-    ax.set_ylabel('Price')
-
-    fig.tight_layout()
-
+    fig, ax = mpf.plot(df, type='candle', style='yahoo', returnfig=True)
+    print(df.info())
+    print(levels)
+    
     for level in levels:
-        plt.hlines(level[1],xmin=df['Date'].iloc[level[0]], xmax=max(df['Date']),colors='black', alpha=0.6)
-        
-    plt.savefig('static/my_plot.png')
-    plt.close(fig)  
+        ax[0].axhline(y=level[1], color='black', linestyle='--', alpha=0.6)
+    
+    fig.savefig('static/my_plot.png')
+    plt.close(fig)
 
 @app.route('/')
 def hello():
     return render_template('index.html')
 
-@app.route('/get_plot', methods=['GET','POST'])
+@app.route('/get_plot', methods=['GET', 'POST'])
 def get_plot():
     if request.method == "POST":
         stock = request.form['stock_name']
